2f73e61d0770ed829635bc97ee621b578d140f2a
DangDuyy <duyprovn987@gmail.com>
2025-08-04 22:18:16 +0700

    apply neo và prisma orm để dùng postgre onl

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,8 @@
-import ModeToggle from '@/components/ModeToggle'
-import { Button } from '@/components/ui/button'
-import {
-  SignInButton,
-  SignedIn,
-  SignedOut,
-  UserButton
-} from '@clerk/nextjs'
 
 export default function Home() {
   return (
     <div className='m-4'>
-      <SignedOut>
-        <SignInButton mode='modal' >
-          <Button>
-            Sign in
-          </Button>
-        </SignInButton>
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-      <ModeToggle/>
-      <Button variant={'secondary'}> Click me</Button>
+      <h1>Home page content</h1>
     </div>
   )
 }
