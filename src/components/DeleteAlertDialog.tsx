b0becac14ea5b88981b35503b755881b55b06d2f
DangDuyy <duyprovn987@gmail.com>
2025-08-07 19:53:23 +0700

    delete post

--- src/components/DeleteAlertDialog.tsx
+++ src/components/DeleteAlertDialog.tsx
@@ -1,10 +1,51 @@
 "user client"
 
-import React from 'react'
+import { AlertDialog, AlertDialogTitle, AlertDialogTrigger } from "@radix-ui/react-alert-dialog"
+import { Button } from "./ui/button"
+import { Loader2Icon, Trash2Icon } from "lucide-react"
+import { AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader } from "./ui/alert-dialog"
 
-function DeleteAlertDialog( { isDeleting: string }) {
+interface DeleteAlertDialogProps {
+  isDeleting: boolean 
+  onDelete: () => Promise<void>
+  title?: string
+  description?: string
+}
+
+function DeleteAlertDialog( { isDeleting, onDelete, title = "Delete Post", description = "This action can't be undone." } : DeleteAlertDialogProps) {
   return (
-    <div>DeleteAlertDialog</div>
+    <AlertDialog>
+      <AlertDialogTrigger asChild>
+        <Button
+          variant="ghost"
+          size="sm"
+          className="text-muted-foreground hover:text-red-500 -mr-2"
+        >
+          {isDeleting ? (
+            <Loader2Icon className="size-4 animate-spin"/> 
+          ) : (
+            <Trash2Icon className="size-4" />
+          )
+        }
+        </Button>
+      </AlertDialogTrigger>
+
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle> {title} </AlertDialogTitle>
+          <AlertDialogDescription> {description} </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction
+            onClick={onDelete}
+            className="bg-red-500 hover:bg-red-600"
+          >
+            {isDeleting ? "Deleting..." : "Delete"}
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
   )
 }
 
